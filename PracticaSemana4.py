e87b87c9da5c9c7e94dc398f308a573104da590e
cupena <cupena@uamv.edu.ni>
2026-09-07 08:18:49 -0600

    ejercicio practico

---
+++ PracticaSemana4.py
@@ -0,0 +1,36 @@
+"""Desarrollen un programa para una ferretería que cumpla estos requerimientos:
+
+Solicitar el nombre del producto, precio y cantidad.
+Crear una función que calcule el subtotal.
+Crear una función que aplique 8 % de descuento cuando el subtotal sea mayor o igual a C$ 3,000.
+Crear una función que calcule el IVA del 15 % después del descuento.
+Crear un procedimiento que muestre producto, subtotal, descuento, IVA y total.
+Utilizar variables locales dentro de las funciones.
+Probar el programa con una compra que reciba descuento y otra que no lo reciba.
+Explicar qué parámetros presentan comportamiento similar al paso por valor."""
+
+nombreProducto = input("Ingrese el nombre del producto: ")
+precioProducto = float(input("Ingrese el precio del producto: "))
+cantidadProducto = int(input("Ingrese cuanto llevara: "))
+descuento = 0.08
+
+def calcular_subtotal(precioProducto, cantidadProducto):
+    subtotal = precioProducto * cantidadProducto
+    return subtotal
+
+def aplicar_descuento(subtotal, descuento):
+    descuentado = subtotal * descuento
+    return descuentado
+
+def aplicar_impuesto(subtotal, descuentado):
+    impuesto = (subtotal - descuentado) * 0.15
+    return impuesto
+def calcular_total(subtotal, descuentado, impuesto):
+    total = (subtotal - descuentado) + impuesto
+    return total
+
+def mostrar_listado(subtotal, descuentado,  impuesto, total):
+    print (f"El subtotal es: C${subtotal}, se le aplica un descuento de: C${descuentado}, pagando un IVA de: C${impuesto} para un total de: C${total})
+    return 
+
+    
